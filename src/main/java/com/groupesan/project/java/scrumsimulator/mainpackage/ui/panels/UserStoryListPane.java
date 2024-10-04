e6552cb96dacb81c8e4d58caabe9c72f2f8ad30a
unknown <pbelegur@asu.edu>
2024-10-03 19:02:54 -0700

    Add a new column for getting the status.
    Added a new column to display the status of the
    tasks. Set to new for now.
    
    Signed-off-by: unknown <pbelegur@asu.edu>

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/UserStoryListPane.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/UserStoryListPane.java
@@ -34,14 +34,6 @@ public class UserStoryListPane extends JFrame implements BaseComponent {
         myJpanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         myJpanel.setLayout(myGridbagLayout);
 
-        // demo/testing widgets
-        //        UserStory aUserStory = UserStoryFactory.getInstance().createNewUserStory("foo",
-        // "bar", 2);
-        //        UserStory aUserStory2 =
-        //                UserStoryFactory.getInstance().createNewUserStory("foo2", "bar2", 4);
-        //        widgets.add(new UserStoryWidget(aUserStory));
-        //        widgets.add(new UserStoryWidget(aUserStory2));
-
         for (UserStory userStory : UserStoryStore.getInstance().getUserStories()) {
             widgets.add(new UserStoryWidget(userStory));
         }
