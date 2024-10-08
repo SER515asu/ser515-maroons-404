39df2a0dfd7356dad9d19bcb97adebe2518c3700
rkulka38 <rkulka38@asu.edu>
2024-10-08 11:05:03 -0700

    Debugging Test for US_17_task_37
    Removing previous changes to debug from scratch.
    
    Signed-off-by: rkulka38 <rkulka38@asu.edu>

--- src/test/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/DemoPaneTest.java
+++ src/test/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/DemoPaneTest.java
@@ -1,11 +1,6 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage.ui.panels;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.awt.Component;
-import javax.swing.JButton;
-import javax.swing.JPanel;
 import org.junit.jupiter.api.Test;
 
 public class DemoPaneTest {
@@ -19,31 +14,5 @@ public class DemoPaneTest {
     } catch (ClassNotFoundException e) {
       assertTrue(false, "DemoPane class does not exist");
     }
-  }
-
-  @Test
-  public void testNewSimulationButtonExistence() {
-    DemoPane demoPane = new DemoPane();
-    Component[] components = demoPane.getContentPane().getComponents();
-    JPanel myJpanel = null;
-    for (Component component : components) {
-      if (component instanceof JPanel) {
-        myJpanel = (JPanel) component;
-        break;
-      }
-    }
-    assertNotNull(myJpanel, "'myJpanel' should exist in DemoPane.");
-    Component[] panelComponents = myJpanel.getComponents();
-    JButton newSimulationButton = null;
-    for (Component component : panelComponents) {
-      if (component instanceof JButton) {
-        JButton button = (JButton) component;
-        if (button.getText().equals("Create New Simulation")) {
-          newSimulationButton = button;
-          break;
-        }
-      }
-    }
-    assertNotNull(newSimulationButton, "'Create New Simulation' button should exist in myJpanel.");
   }
 }
