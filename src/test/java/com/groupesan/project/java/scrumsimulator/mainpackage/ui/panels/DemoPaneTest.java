0e6b138310a08cb5dcb032a0e335f5680480d9a5
rkulka38 <rkulka38@asu.edu>
2024-10-08 13:05:50 -0700

    Test implementation for US_17_task_37
    Signed-off-by: rkulka38 <rkulka38@asu.edu>

--- src/test/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/DemoPaneTest.java
+++ src/test/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/DemoPaneTest.java
@@ -1,19 +1,18 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage.ui.panels;
 
-import java.awt.Component;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test; 
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import org.junit.jupiter.api.Test;
 
 public class DemoPaneTest {
 
   @Test
   public void testPaneExistence() {
-    
+
     try {
       Class.forName("com.groupesan.project.java.scrumsimulator.mainpackage.ui.panels.DemoPane");
       assertTrue(true, "DemoPane class exists");
@@ -21,9 +20,10 @@ public class DemoPaneTest {
       assertTrue(false, "DemoPane class does not exist");
     }
   }
+
   @Test
   public void testCreateNewSimulationButtonExists() {
-    
+
     DemoPane demoPane = new DemoPane();
     Component[] components = demoPane.getContentPane().getComponents();
     JPanel myJpanel = null;
@@ -47,10 +47,8 @@ public class DemoPaneTest {
       }
     }
     assertNotNull(newSimulationButton, "'Create New Simulation' button should exist in myJpanel.");
-
   }
 
-  
   private JButton findButtonByText(DemoPane pane, String text) {
     for (java.awt.Component component : pane.getContentPane().getComponents()) {
       if (component instanceof JButton) {
