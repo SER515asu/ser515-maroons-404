8e1a3f771a6f74e78ceff994c2f4df03e538dc16
rkulka38 <rkulka38@asu.edu>
2024-10-10 20:36:50 -0700

    Test task update
    Updated testcase for US_20_task_46
    
    Signed-off-by: rkulka38 <rkulka38@asu.edu>

--- src/test/java/com/groupesan/project/java/scrumsimulator/mainpackage/impl/ProductBacklogStoreTest.java
+++ src/test/java/com/groupesan/project/java/scrumsimulator/mainpackage/impl/ProductBacklogStoreTest.java
@@ -3,14 +3,12 @@ package com.groupesan.project.java.scrumsimulator.mainpackage.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 public class ProductBacklogStoreTest {
     ProductBacklogStore productbacklog ;
@@ -25,8 +23,15 @@ public class ProductBacklogStoreTest {
         try (MockedStatic<UserStoryStore> mockedStore = mockStatic(UserStoryStore.class)) {
             UserStoryStore mockUserStoryStore = mock(UserStoryStore.class);
             List<UserStory> userstorieslist = new ArrayList<UserStory>();
-            UserStory userstory = new UserStory("new", 0);
-           // when(mockUserStoryStore.getUserStories()).thenReturn();
+            UserStory userstory1 = new UserStory("XYZ", "ABC",2.0,"new");
+            userstory1.doRegister();
+            
+            UserStory userstory2 = new UserStory("Rachana Rocks", "That is not true",2.0,"completed");
+            userstory2.doRegister();
+            userstorieslist.add(userstory1);
+            userstorieslist.add(userstory2);
+            when(mockUserStoryStore.getUserStories()).thenReturn(userstorieslist);
             mockedStore.when(UserStoryStore::getInstance).thenReturn(mockUserStoryStore);
     }
+}
 }
