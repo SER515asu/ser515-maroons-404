56bf1990a74783123a8e978ca3d3e9e7c64f5926
Pavan Samarth Vittal Bele Gururaja <pbelegur@asu.edu>
2024-10-29 22:46:46 -0700

    Commit to add developers drop down menu
    Added developers dropdown menu
    
    Signed-off-by: Pavan Samarth Vittal Bele Gururaja <pbelegur@asu.edu>

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/SimulationPane.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/SimulationPane.java
@@ -1,7 +1,9 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage.ui.panels;
 
-import com.groupesan.project.java.scrumsimulator.mainpackage.impl.AddUser;
+import com.groupesan.project.java.scrumsimulator.mainpackage.impl.DeveloperStore;
+import com.groupesan.project.java.scrumsimulator.mainpackage.utils.CustomConstraints;
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,13 +11,14 @@ import java.util.Arrays;
 import java.util.List;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 /**
  * SimulationPane is a part of the UI in the scrum simulator.
@@ -27,7 +30,7 @@ import javax.swing.JTextField;
  */
 public class SimulationPane extends JFrame {
   private JButton joinButton;
-  private JTextField usernameField;
+  private JPopupMenu developerNameField;
   private JRadioButton playerRadioButton;
   private JRadioButton teacherRadioButton;
   private ButtonGroup typeButtonGroup;
@@ -44,30 +47,39 @@ public class SimulationPane extends JFrame {
     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
     JPanel panel = new JPanel();
-    panel.setLayout(new GridLayout(4, 2));
+    panel.setLayout(new GridLayout());
 
-    JLabel usernameLabel = new JLabel("Developer Name:");
-    usernameField = new JTextField(20);
-    panel.add(usernameLabel);
-    panel.add(usernameField);
+    JLabel developerLabel = new JLabel("Developer Name:");
 
-    /*JLabel typeLabel = new JLabel("Type:");
-    panel.add(typeLabel);
+    developerNameField = new JPopupMenu();
+    List<String> devList = DeveloperStore.getInstance().getDeveloperList();
+    for (String developerName : devList) {
+      JCheckBox jCheckBox = new JCheckBox(developerName);
+      developerNameField.add(jCheckBox);
+    }
 
-    typeButtonGroup = new ButtonGroup();
-    playerRadioButton = new JRadioButton("Player");
-    teacherRadioButton = new JRadioButton("Teacher");
-    typeButtonGroup.add(playerRadioButton);
-    typeButtonGroup.add(teacherRadioButton);
+    JButton dropdownButton = new JButton("Select Options");
+    dropdownButton.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            developerNameField.show(dropdownButton, 3, dropdownButton.getHeight());
+          }
+        });
+    panel.setBorder(new EmptyBorder(50, 10, 50, 10));
 
-    panel.add(playerRadioButton);
-    panel.add(new JLabel(""));
-    panel.add(teacherRadioButton);
-
-    JLabel roleNameLabel = new JLabel("Role Name:");
-    roleComboBox = new JComboBox<>(allowedRoleNames.toArray(new String[0]));
-    panel.add(roleNameLabel);
-    panel.add(roleComboBox);*/
+    panel.add(
+        developerLabel,
+        new CustomConstraints(
+            1, 0, GridBagConstraints.WEST, 1.0, 1.0, GridBagConstraints.HORIZONTAL));
+    panel.add(
+        dropdownButton,
+        new CustomConstraints(
+            3, 0, GridBagConstraints.WEST, 1.0, 1.0, GridBagConstraints.HORIZONTAL));
+    panel.add(
+        developerNameField,
+        new CustomConstraints(
+            3, 1, GridBagConstraints.WEST, 1.0, 1.0, GridBagConstraints.HORIZONTAL));
 
     joinButton = new JButton("Add Developer");
     joinButton.addActionListener(
@@ -75,33 +87,31 @@ public class SimulationPane extends JFrame {
           @Override
           public void actionPerformed(ActionEvent e) {
             // Logic for join button
-            onJoinButtonClicked();
+            // onJoinButtonClicked();
           }
         });
 
     setLayout(new BorderLayout());
-    add(joinButton, BorderLayout.SOUTH);
     add(panel);
   }
 
-  private void onJoinButtonClicked() {
-    String username = usernameField.getText();
-    /*String type = playerRadioButton.isSelected() ? "player" : "teacher";
-    String roleName = roleComboBox.getSelectedItem().toString();*/
+  // private void onJoinButtonClicked() {
+  // String username = developerNameField.getText();
+  /*String type = playerRadioButton.isSelected() ? "player" : "teacher";
+  String roleName = roleComboBox.getSelectedItem().toString();*/
 
-    if (username.isEmpty()) {
-      JOptionPane.showMessageDialog(
-          null, "Developer name cannot be empty", "Error", JOptionPane.ERROR_MESSAGE);
-      return;
-    }
+  // if (username.isEmpty()) {
+  //   JOptionPane.showMessageDialog(
+  //       null, "Developer name cannot be empty", "Error", JOptionPane.ERROR_MESSAGE);
+  //   return;
+  // }
 
-    AddUser.addUser(username);
-    clearFields();
-  }
-
-  private void clearFields() {
-    usernameField.setText("");
-    // typeButtonGroup.clearSelection();
-    // roleComboBox.setSelectedIndex(0);
-  }
+  // AddUser.addUser(username);
+  // clearFields();
 }
+
+  // private void clearFields() {
+  //   developerNameField.setText("");
+  //   // typeButtonGroup.clearSelection();
+  //   // roleComboBox.setSelectedIndex(0);
+  // }
